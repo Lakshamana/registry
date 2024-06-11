c2833b9c7d9c12455f76cdeeb8bfc79c44d57faa
Lakhshamana <lakshamana@protonmail.com>
2024-06-11 09:14:21 -0300

    chore: expose decorators + registry

---
+++ index.ts
@@ -0,0 +1,1 @@
+export { Registry, Inject, Injectable, InjectVariable } from './src/core'
--- src/core/index.ts
+++ src/core/index.ts
@@ -1,1 +1,3 @@
 export * from './instance-loader'
+export * from './instance-registry'
+export * from './decorators'
